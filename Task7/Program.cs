f3bf6201fef10d35724fb671354129f5b46d6907
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-01 19:51:17 +0400

    Task 7 is done

---
+++ Task7/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program Check Voting Eligibility");
+
+            // ask user to enter age
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            string category;
+            double price;
+            if (age <= 12)
+            {
+                category = "Children";
+                price = 2.000;
+            }
+            else if (age >= 60)
+            {
+                category = "Seniors";
+                price = 3.000;
+            }
+            else
+            {
+                category = "Adults";
+                price = 5.000;
+            }
+            Console.WriteLine("Category: " + category + ", price: " + price + " OMR");
+        }
+    }
+}
