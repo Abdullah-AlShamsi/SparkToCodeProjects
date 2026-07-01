eac170191523cf1a96cb3315d5b9d1c03efb563e
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-01 12:12:53 +0400

    Task 2 is done

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculate the area and perimeter of rectangle");
+            
+            // ask user to enter length
+            Console.Write("Enter the length of rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+
+            // ask user to enter width
+            Console.Write("Enter the width of rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = length * width; //calculate the area
+
+            double perimeter = 2 * (length + width); //calculate the perimeter
+
+            //Print the area and perimeter of rectangle
+            Console.WriteLine("The area of rectangle is " + area);
+            Console.WriteLine("The perimeter of rectangle is " + perimeter);
+
+
+        }
+    }
+}
