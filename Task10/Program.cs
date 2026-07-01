5c65ac84cdd023734583b6bc29addc75ed480812
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-01 20:37:39 +0400

    task 10 is done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -6,13 +6,55 @@
         {
             Console.WriteLine("This program calculate the area and perimeter of rectangle");
 
-            // ask user to enter length
-            Console.Write("Enter the length of rectangle: ");
-            double length = double.Parse(Console.ReadLine());
+            // ask user to enter num1
+            Console.Write("Enter first number: ");
+            double num1 = double.Parse(Console.ReadLine());
 
-            // ask user to enter width
-            Console.Write("Enter the width of rectangle: ");
-            double width = double.Parse(Console.ReadLine());
+            // ask user to enter operator
+            Console.Write("Enter operator (+, -, *, /, or %): ");
+            string operator_cher = Console.ReadLine();
+
+            // ask user to enter num2
+            Console.Write("Enter second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+            double result;
+            switch (operator_cher)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine(num1 + operator_cher + num2 + "=" + result );
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine(num1 + operator_cher + num2 + "=" + result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine(num1 + operator_cher + num2 + "=" + result);
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                        break;
+                    }
+                    result = num1 / num2;
+                    Console.WriteLine(num1 + operator_cher + num2 + "=" + result);
+                    break;
+                case "%":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                        break;
+                    }
+                    result = num1 % num2;
+                    Console.WriteLine(num1 + operator_cher + num2 + "=" + result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+
         }
     }
 }
