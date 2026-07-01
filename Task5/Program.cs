f6960c5897c0cb96d3f474ac7635ef66ab5aee6c
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-01 14:44:31 +0400

    Task 5 is done

---
+++ Task5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program For Grade Letter Lookup");
+
+            // ask user to enter grade
+            Console.Write("Enter your grade as single character ('A', 'B', 'C', 'D', or 'F'): ");
+            char grade = char.Parse(Console.ReadLine());
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Very Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Pass");
+                    break;
+                case 'F':
+                    Console.WriteLine("Fail");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Grade");
+                    break;
+            }
+
+
+        }
+    }
+}
