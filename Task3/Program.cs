c9fb31e2bf7c3192f61432455b3f02709a6b759b
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-01 12:24:00 +0400

    Task 3 is done

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Even or Odd Checker");
+            
+            //read the number
+            Console.Write("Enter a whole number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int mod = num % 2;
+
+            if (mod == 0)
+            {
+                Console.WriteLine("Your number is even");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd");
+            }
+
+        }
+    }
+}
