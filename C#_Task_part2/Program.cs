81d948c271cc78c12236b9d9bba42fdc44b540cf
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-02 11:35:48 +0400

    Task 1 is done

--- C#_Task_part2/Program.cs
+++ C#_Task_part2/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // task 1
+            Console.WriteLine("This Countdown Timer");
+
+            // ask user to enter a product code
+            Console.Write("Enter a starting number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i = number; i>0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
         }
     }
 }
