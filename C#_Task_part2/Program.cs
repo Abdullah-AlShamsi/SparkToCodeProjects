0fdb05bdbbb8a66d39b7c5115d779a513434e494
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-02 11:45:23 +0400

    task 2 is done

--- C#_Task_part2/Program.cs
+++ C#_Task_part2/Program.cs
@@ -8,7 +8,7 @@
             //// task 1
             //Console.WriteLine("This Countdown Timer");
 
-            //// ask user to enter a product code
+            //// ask user to enter a starting number
             //Console.Write("Enter a starting number: ");
             //int number = int.Parse(Console.ReadLine());
 
@@ -17,6 +17,23 @@
             //    Console.WriteLine(i);
             //}
             //Console.WriteLine("Liftoff!");
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+            // task 2
+            Console.WriteLine("Sum of Numbers 1 to N");
+
+            // ask user to enter a positive whole number N
+            Console.Write("a positive whole number N: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The final sum = " + sum);
             ////////////////////////////////////////////////////////////////////////////////////////////////
         }
     }
