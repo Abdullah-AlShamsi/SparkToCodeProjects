8e42efa692552d1ba3de0ad8463c03b2945191fb
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-05 21:48:28 +0400

    comment out task 1

--- C#_Task_part3/Program.cs
+++ C#_Task_part3/Program.cs
@@ -5,24 +5,22 @@
         static void Main(string[] args)
         {
             ////////////////////////////////////////////////////////////////////////////////////////////////
-            // task 1
-            Console.WriteLine("Absolute Difference");
+            //// task 1
+            //Console.WriteLine("Absolute Difference");
 
-            // ask user to enter num1
-            Console.Write("Enter first number: ");
-            int num1 = int.Parse(Console.ReadLine());
+            //// ask user to enter num1
+            //Console.Write("Enter first number: ");
+            //int num1 = int.Parse(Console.ReadLine());
 
-            // ask user to enter num2
-            Console.Write("Enter second number: ");
-            int num2 = int.Parse(Console.ReadLine());
+            //// ask user to enter num2
+            //Console.Write("Enter second number: ");
+            //int num2 = int.Parse(Console.ReadLine());
 
-            int sub_value = num1 - num2;
+            //int sub_value = num1 - num2;
 
-            int abs_value = Math.Abs(sub_value);
+            //int abs_value = Math.Abs(sub_value);
 
-            Console.WriteLine("The absolute difference result of your numbers is " + abs_value);
-
-
+            //Console.WriteLine("The absolute difference result of your numbers is " + abs_value);
             ////////////////////////////////////////////////////////////////////////////////////////////////
         }
     }
