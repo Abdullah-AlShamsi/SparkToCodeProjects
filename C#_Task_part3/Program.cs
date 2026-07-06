7b1fb9e7f6e19abf0474897cb7b405077a204023
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-06 11:02:09 +0400

    task 11 is done

--- C#_Task_part3/Program.cs
+++ C#_Task_part3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Net.NetworkInformation;
+using System.Xml.Linq;
 
 namespace C__Task_part3
 {
@@ -229,6 +230,50 @@ namespace C__Task_part3
             //}
             ////////////////////////////////////////////////////////////////////////////////////////////////
 
-        }
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+            //// task 11
+            Console.WriteLine("One-Time Password (OTP) Generator");
+            Random random = new Random();
+
+            int rund_otp = random.Next(1000, 10000);
+            
+
+            Console.WriteLine("Do not share this number with anyone, otp: " + rund_otp);
+            bool isValid = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                // ask the user to type the code back to verify it.
+                Console.Write("Enter the OTP: ");
+                try
+                {
+                    int inOTP = int.Parse(Console.ReadLine());
+                    if (inOTP == rund_otp)
+                    {
+                        isValid = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect.");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+            if (isValid)
+            {
+                Console.WriteLine("Verified");
+            }
+            else
+            {
+                Console.WriteLine("Verification Failed"); 
+            }
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+
+            }
     }
 }
