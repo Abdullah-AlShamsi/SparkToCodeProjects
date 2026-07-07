9970d4bc7300333ed14d4f10e969ddfb919ff354
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-07 17:58:23 +0400

    task 2 is done

--- C#_Task_part4/Program.cs
+++ C#_Task_part4/Program.cs
@@ -7,15 +7,33 @@
         {
             Console.WriteLine("Welcome " + name);
         }
+
+        //task 2
+        public static int Square(int num)
+        {
+            return num*num ;
+        }
         static void Main(string[] args)
         {
-
-            //// task 1
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /// task 1
             //// ask user to enter their name
             //Console.WriteLine("Personalized Welcome Function");
             //Console.Write("Enter your name: ");
             //string name = Console.ReadLine();
             //PrintWelcome(name);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///task 2
+            Console.WriteLine("Square Number Function");
+
+            // ask user to enter num1
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Square of your number is :" + Square(num));
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
     }
 }
