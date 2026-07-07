74729fd039adccffdcf038f0bfd36a79a58636c1
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-07 17:50:19 +0400

    task 1 is done

--- C#_Task_part4/Program.cs
+++ C#_Task_part4/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        //task 1
+        public static void PrintWelcome(string name)
+        {
+            Console.WriteLine("Welcome " + name);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // task 1
+            // ask user to enter their name
+            Console.WriteLine("Personalized Welcome Function");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            PrintWelcome(name);
         }
     }
 }
