d79d116dd2858b38efc1e3d15648669440cb1aad
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-08 18:53:57 +0400

    task 1 is done

---
+++ C#_Task_part5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace C__Task_part5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /// task 1
+            Console.WriteLine("Fixed Grades Array");
+
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                /// ask user to enter each grade
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            int j = 1;
+            foreach (int grade in grades)
+            {
+                Console.WriteLine("Grade " + j + ": " + grade);
+                j++;
+            }
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+    }
+}
