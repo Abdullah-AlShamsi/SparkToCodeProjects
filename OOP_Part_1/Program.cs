ab50e31f9bcdfd1640d92a32de27c0c4ce7ebc1e
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-13 15:18:30 +0400

    Class Bank Account with Methods: 1- Deposit(double amount) 2- Withdraw(double amount) 3- CheckBalance() 4- PrintInformation() 5- SendEmail()

---
+++ OOP_Part_1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace OOP_Part_1
+{
+    class BankAccount
+    {
+        public int AccountNumber {get; set;}
+        public string HolderName { get; set; }
+        public double Balance { get; set; }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                SendEmail();
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be a positive number.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                SendEmail();
+            }
+            else
+            {
+                Console.WriteLine("Withdraw amount must be a positive number.");
+            }
+        }
+
+        public double CheckBalance()
+        {
+            PrintInformation();
+            return Balance;
+        }
+
+        void PrintInformation()
+        {
+            Console.WriteLine("Account Holder Name: " + HolderName);
+            Console.WriteLine("Account balance: " + Balance);
+        }
+
+        void SendEmail()
+        {
+            Console.WriteLine("Operation successful, account balance now:" + Balance);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
