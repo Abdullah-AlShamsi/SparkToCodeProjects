7fe2841255c1d14168476012948ad296ca6667b4
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-15 22:50:58 +0400

    declare Lists (rooms, guests) and Pre-load rooms

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -76,7 +76,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Room> rooms = new List<Room>();
+            List<Guest> guests = new List<Guest>();
+
+            rooms.Add(new Room(1, "Single", 20, true));
+            rooms.Add(new Room(2, "Double", 30, true));
+            rooms.Add(new Room(3, "Single", 20, true));
+            rooms.Add(new Room(4, "Suite", 50, true));
+            rooms.Add(new Room(5, "Double", 30, true));
+            rooms.Add(new Room(6, "Double", 30, true));
         }
     }
 }
