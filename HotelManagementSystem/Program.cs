6963d15ac9a0bd1a761a316e61d7e010df829c9e
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-15 22:36:09 +0400

    class Guest

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -33,6 +33,45 @@
             
         }
     }
+
+
+    class Guest
+    {
+        public int guestId { get; }
+        public string guestName { get; set; }
+        public int roomNumber { get; set; }
+        public DateTime checkInDate { get; set; }
+
+        public int totalNights { get; set; }
+
+
+        public Guest(int id, string name, int roomNum, DateTime dayCheckIN, int total)
+        {
+            guestId = id;
+            guestName = name;
+            roomNumber = roomNum;
+            checkInDate = dayCheckIN;
+            totalNights = total;
+
+        }
+
+        public void displayGuest()
+        {
+            Console.WriteLine("Guest Id: " + guestId);
+            Console.WriteLine("Guest Name: " + guestName);
+            Console.WriteLine("Room Number: " + roomNumber);
+            Console.WriteLine("Check In Date: " + checkInDate.ToString("yyyy-MM-dd"));
+            Console.WriteLine("Total Nights: " + totalNights);
+
+
+        }
+
+        public double calculateTotalCost(Room room)
+        {
+            return room.pricePerNight * totalNights; 
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
