67e3bb5d01b859c80de7e86a77e136174e1a0723
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-16 12:59:17 +0400

    move Room claas to separate file

---
+++ HotelManagementSystem/Room.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagementSystem
+{
+    internal class Room
+    {
+        public int roomNumber { get; set; }
+        public string roomType { get; set; }
+        public double pricePerNight { get; set; }
+        public bool isAvailable { get; set; }
+
+
+        public Room(int number, string type, double price, bool availablity)
+        {
+            roomNumber = number;
+            roomType = type;
+            pricePerNight = price;
+            isAvailable = availablity;
+
+        }
+
+        public void displayRoom()
+        {
+            Console.WriteLine("Room Number: " + roomNumber);
+            Console.WriteLine("Room Type: " + roomType);
+            Console.WriteLine("Price Per Night: " + pricePerNight);
+            if (isAvailable)
+            {
+                Console.WriteLine("Is Available: Available");
+            }
+            else
+            {
+                Console.WriteLine("Is Available: Not Available");
+            }
+
+        }
+    }
+}
