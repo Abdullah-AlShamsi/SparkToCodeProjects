67292aed61cf9bd4a4baaca095c18837486a6d5f
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-22 22:29:46 +0400

    Employee Table Creation

--- Database_Task3/CompanyDB.sql
+++ Database_Task3/CompanyDB.sql
@@ -1,2 +1,18 @@
 --Part 1 — Database & Table Creation
 CREATE DATABASE CompanyDB; 
+
+
+--Employee Table Creation
+CREATE TABLE Employee(
+	Ssn int PRIMARY KEY,
+	Fname VARCHAR(50) Not NULL,
+	Minit VARCHAR(50) ,
+	Lname VARCHAR(50) Not NULL,
+	Address VARCHAR(50) Not NULL,
+	Sex VARCHAR(1) NOT NULL,
+	Bdate DATE NOT NULL,
+	Salary DECIMAL(10,2)
+	supervisor_ID int FOREIGN KEY,
+	department_NUM int FOREIGN KEY,
+
+)
