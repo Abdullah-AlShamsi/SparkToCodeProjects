3b638a829caa33fef31852104a4ba78b60d549af
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-27 22:12:43 +0400

    link the project to DB

---
+++ EFCore_practice/ProjectContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_practice
+{
+    internal class ProjectContext : DbContext //oop inhertance
+    {
+    }
+}
