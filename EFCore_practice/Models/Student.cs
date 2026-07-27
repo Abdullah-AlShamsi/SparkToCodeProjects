1ac7f69dcc7ad6b9243f79cc8c9158e5f01d2419
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-27 21:38:00 +0400

    install packages and create file Models for classes

---
+++ EFCore_practice/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_practice.Models
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public double grade { get; set; }
+    }
+}
