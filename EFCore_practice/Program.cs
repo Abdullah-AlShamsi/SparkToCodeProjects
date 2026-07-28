ce7696bbdb571cc35ade814b928b7b8304329487
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-28 11:18:43 +0400

    add student to database

--- EFCore_practice/Program.cs
+++ EFCore_practice/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace EFCore_practice
+﻿using EFCore_practice.Models;
+using System.Security.Cryptography;
+
+namespace EFCore_practice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProjectContext context = new ProjectContext();
+            //add data on table student
+            Student s1 = new Student();
+            s1.name = "Abdullah";
+            s1.grade = 100;
+            s1.age = 22;
+            context.students.Add(s1);
+            context.SaveChanges();
         }
     }
 }
