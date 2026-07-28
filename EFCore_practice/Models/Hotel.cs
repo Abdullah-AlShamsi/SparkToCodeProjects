64c41c56cdfe79a809479b4b1af96f350563497a
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-28 20:29:14 +0400

    hotel class

---
+++ EFCore_practice/Models/Hotel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_practice.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string name { get; set; }
+        public double location { get; set; }
+    }
+}
