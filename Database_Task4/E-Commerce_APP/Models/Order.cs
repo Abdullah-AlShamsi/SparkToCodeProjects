6e228ed201c8aef8cf24f3109f9dcedc0140b3e3
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-29 10:55:08 +0400

    class Order

---
+++ Database_Task4/E-Commerce_APP/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce_APP.Models
+{
+    public class Order
+    {
+        [Key]
+        public int order_ID { get; set; }
+
+        public DateTime date { get; set; }
+
+    }
+}
