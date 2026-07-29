3c8d8872ad99deefdc21eece8c9605df9aca06bc
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-29 10:56:54 +0400

    class Review

---
+++ Database_Task4/E-Commerce_APP/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce_APP.Models
+{
+    public class Review
+    {
+        [Key]
+        public int review_ID { get; set; }
+
+        public double rating { get; set; }
+        public string comment { get; set; }
+    }
+}
