e84e061c84e2dd0766b84065b4235b9edc83f4a3
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-29 10:53:42 +0400

    user class

---
+++ Database_Task4/E-Commerce_APP/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce_APP.Models
+{
+    public class User
+    {
+        [Key]
+        public int user_ID { get; set; }
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
