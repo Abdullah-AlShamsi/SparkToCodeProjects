d73063fbbbb2de4dac1a278bf0a85f944ea35380
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-07-29 11:00:11 +0400

    class Category and class Product

---
+++ Database_Task4/E-Commerce_APP/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce_APP.Models
+{
+    public class Category
+    {
+        [Key]
+        public int category_ID { get; set; }
+
+        public string name { get; set; }
+    }
+}
---
+++ Database_Task4/E-Commerce_APP/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce_APP.Models
+{
+    public class Product
+    {
+        [Key]
+        public int product_ID { get; set; }
+
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
